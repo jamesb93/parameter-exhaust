29ab4bccd724f24c810590d3adae0906752796b7
James Bradbury <jamesbradbury93@gmail.com>
2021-07-22 18:04:29 +0100

    shift functions bottom

--- exhaust.js
+++ exhaust.js
@@ -6,6 +6,80 @@ let resolution = 3;
 let cartProd = [];
 let internalCounter = 0;
 
+m.addHandler('dump', () => {
+	m.outlet('dump', parameters);
+})
+
+m.addHandler('parameter', (name, min, max) => {
+	// Create a new parameter configuration;
+	let _min = min;
+	let _max = max;
+
+	if (min > max) {
+		_min = max,
+		_max = min
+	}
+
+	parameters[name] = {
+		min: _min,
+		max: _max
+	};
+});
+
+m.addHandler('resolution', x => {
+	// How many steps to divide each parameter range into;
+	resolution = x;
+});
+
+m.addHandler('calculate', () => {
+	// Calculate the cartesian products of all the parameters;
+	let allParams = [];
+	for (const [key, value] of Object.entries(parameters)) {
+		const paramSteps = range(value.min, value.max);
+		parameters[key]['steps'] = paramSteps;
+		allParams.push(paramSteps)
+	};
+	cartProd = product(allParams);
+	m.post(cartProd.length, 'parameter combinations!')
+});
+
+m.addHandler('iterate', () => {
+	if (internalCounter === cartProd.length) {
+		m.outlet('done')
+	} else {
+		// Output the current status
+		m.outlet(internalCounter);
+		// move to the next parameter configuration
+		const chosenParams = cartProd[internalCounter];
+		// now prepend the parameter names to the indices.
+
+		chosenParams.forEach((p, idx) => {
+			m.outlet(Object.keys(parameters)[idx], p)
+		})
+		
+		internalCounter += 1;
+	}
+})
+
+m.addHandler('clear', () => {
+	// Clear the parameters
+	parameters = {};
+	cartProd = [];
+});
+
+m.addHandler('reset', () => {
+	internalCounter = 0;
+})
+
+m.addHandler('set', i => {
+	internalCounter = i;
+})
+
+m.addHandler('delete', parameter => {
+	delete parameters[parameter];
+});
+
+// INLINE LIBRARY FOR EASE
 const product = (elements) => {
 	if (!Array.isArray(elements)) {
 		throw new TypeError();
@@ -44,70 +118,3 @@ const range = (start, stop) => {
 	const increment = Math.abs(start - stop) / resolution;
 	return Array.from({ length: (stop - start) / increment + 1}, (_, i) => start + (i * increment));
 }
-
-m.addHandler('parameter', (name, min, max) => {
-	// Create a new parameter configuration;
-	let _min = min;
-	let _max = max;
-
-	if (min > max) {
-		_min = max,
-		_max = min
-	}
-
-	parameters[name] = {
-		min: _min,
-		max: _max
-	};
-});
-
-m.addHandler('resolution', (x) => {
-	// How many steps to divide each parameter range into;
-	resolution = x;
-});
-
-m.addHandler('calculate', () => {
-	// Calculate the cartesian products of all the parameters;
-	let allParams = [];
-	for (const [key, value] of Object.entries(parameters)) {
-		const paramSteps = range(value.min, value.max);
-		parameters[key]['steps'] = paramSteps;
-		allParams.push(paramSteps)
-	};
-	cartProd = product(allParams);
-	m.post(cartProd.length, 'parameter combinations!')
-});
-
-m.addHandler('iterate', () => {
-	if (internalCounter === cartProd.length) {
-		m.outlet('done')
-	} else {
-		// move to the next parameter configuration
-		const chosenParams = cartProd[internalCounter];
-		// now prepend the parameter names to the indices.
-
-		chosenParams.forEach((p, idx) => {
-			m.outlet(Object.keys(parameters)[idx], p)
-		})
-
-		internalCounter += 1
-	}
-})
-
-m.addHandler('clear', () => {
-	// Clear the parameters
-	parameters = {};
-	cartProd = [];
-});
-
-m.addHandler('reset', () => {
-	internalCounter = 0;
-})
-
-m.addHandler('set', i => {
-	internalCounter = i
-})
-
-m.addHandler('delete', (parameter) => {
-	delete parameters[parameter];
-});
